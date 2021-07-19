16d071028c629c175be91aa373e242834dc3c4c3
Shazeen15 <shazzy.sterlin@gmail.com>
2021-07-18 17:11:38 -0700

    shuffled the answer options

--- src/App.js
+++ src/App.js
@@ -5,11 +5,77 @@ import triviaQuestions from "./Apprentice_TandemFor400_Data.json";
 
 function App() {
   const [trivia] = useState(triviaQuestions);
-  // console.log(trivia);
+  const [tenQuest] = useState([]);
+  const [selectedAns, setSelectedAns] = useState("");
+  const [currentQuest, setCurrentQuest] = useState(0);
+  const [score, setScore] = useState(0);
+  const [start, setStart] = useState(true);
+  const [correctAns, setCorrectAns] = useState(false);
+  const [wrongAns, setWrongAns] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(false);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [submit, setSubmit] = useState(false);
+
+  const startTrivia = () => {
+    //   still need to make each question unique
+    setStart(false);
+    for (let i = 0; i < 10; i++) {
+      let randoQuest = Math.floor(Math.random() * 10);
+      tenQuest.push(trivia[randoQuest]);
+    }
+  };
+
+  const correctAnswer = (e) => {
+    setSelectedAns(e.target.firstChild.data);
+    if (tenQuest[currentQuest].correct === e.target.firstChild.data) {
+      setCorrectAns(true);
+      setScore(score + 1);
+    } else {
+      setCorrectAns(true);
+      setWrongAns(true);
+    }
+    setBtnDisabled(true);
+    setNextBtnDisabled(false);
+  };
+
+  const handleNextBtn = (e) => {
+    const next = currentQuest + 1;
+    if (next < tenQuest.length) {
+      setCurrentQuest(next);
+    } else {
+      setSubmit(true);
+    }
+    setSelectedAns("");
+    setCorrectAns(false);
+    setWrongAns(false);
+    setBtnDisabled(false);
+    setNextBtnDisabled(true);
+  };
+
+  const retryBtn = () => {
+    window.location.reload();
+  };
+
   return (
     <div>
       <h1>Tandem Trivia</h1>
-      <Questions trivia={trivia} />
+      <Questions
+        startTrivia={startTrivia}
+        handleNextBtn={handleNextBtn}
+        correctAnswer={correctAnswer}
+        retryBtn={retryBtn}
+        start={start}
+        submit={submit}
+        tenQuest={tenQuest[currentQuest]}
+        selectedAns={selectedAns}
+        wrongAns={wrongAns}
+        btnDisabled={btnDisabled}
+        correctAns={correctAns}
+        nextBtnDisabled={nextBtnDisabled}
+        score={score}
+        currentQuest={currentQuest}
+        tenQuests={tenQuest}
+      />
     </div>
   );
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,65 +2,27 @@ import React, { useState } from "react";
 import "./questions.css";
 
 export default function Questions(props) {
-  const { trivia } = props;
-  const [tenQuest] = useState([]);
-  const [selectedAns, setSelectedAns] = useState("");
-  const [currentQuest, setCurrentQuest] = useState(0);
-  const [score, setScore] = useState(0);
-  const [start, setStart] = useState(true);
-  const [correctAns, setCorrectAns] = useState(false);
-  const [wrongAns, setWrongAns] = useState(false);
-  const [btnDisabled, setBtnDisabled] = useState(false);
-  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
-  const [submit, setSubmit] = useState(false);
+  const {
+    startTrivia,
+    handleNextBtn,
+    correctAnswer,
+    retryBtn,
+    start,
+    submit,
+    tenQuest,
+    selectedAns,
+    wrongAns,
+    btnDisabled,
+    correctAns,
+    nextBtnDisabled,
+    score,
+    currentQuest,
+    tenQuests,
+  } = props;
 
-  const startTrivia = () => {
-    //   still need to make each question unique
-    setStart(false);
-    for (let i = 0; i < 10; i++) {
-      let randoQuest = Math.floor(Math.random() * 10);
-      tenQuest.push(trivia[randoQuest]);
-    }
-  };
-
-  console.log(tenQuest);
-  const handleNextBtn = (e) => {
-    const next = currentQuest + 1;
-    if (next < tenQuest.length) {
-      setCurrentQuest(next);
-    } else {
-      setSubmit(true);
-    }
-    setSelectedAns("");
-    setCorrectAns(false);
-    setWrongAns(false);
-    setBtnDisabled(false);
-    setNextBtnDisabled(true);
-  };
-
-  const correctAnswer = (e) => {
-    setSelectedAns(e.target.firstChild.data);
-    if (tenQuest[currentQuest].correct === e.target.firstChild.data) {
-      setCorrectAns(true);
-      setScore(score + 1);
-    } else {
-      setCorrectAns(true);
-      setWrongAns(true);
-    }
-    setBtnDisabled(true);
-    setNextBtnDisabled(false);
-  };
-
-  const retryBtn = () => {
-    setCurrentQuest(0);
-    setScore(0);
-    setStart(true);
-    setSubmit(false);
-    setCorrectAns(false);
-    setWrongAns(false);
-    setBtnDisabled(false);
-    setNextBtnDisabled(true);
-  };
+  const shuffledAnswers =
+    tenQuests.length > 0 &&
+    [...tenQuest.incorrect, tenQuest.correct].sort(() => Math.random() - 0.5);
 
   return (
     <div>
@@ -69,37 +31,32 @@ export default function Questions(props) {
       ) : (
         <>
           {submit ? (
-            ""
+            <div className="score">
+              <h5>
+                You scored {score}/{tenQuests.length}
+              </h5>
+              <button onClick={retryBtn}>Retry</button>
+            </div>
           ) : (
             <>
               <h2>Questions</h2>
               <h2 className="question">
-                {tenQuest[currentQuest].question} {currentQuest + 1}/ 10
+                {tenQuest.question} {currentQuest + 1}/ 10
               </h2>
               <p>
                 Selected Answer:{" "}
                 {selectedAns ? selectedAns : "Please select an answer"}
               </p>
               <div className="answer-options">
-                {tenQuest[currentQuest].incorrect.map((option) => {
-                  return (
-                    <button
-                      key={option}
-                      onClick={correctAnswer}
-                      className={wrongAns ? "wrong" : ""}
-                      disabled={btnDisabled}
-                    >
-                      {option}
-                    </button>
-                  );
-                })}
-                <button
-                  onClick={correctAnswer}
-                  className={correctAns ? "correct" : ""}
-                  disabled={btnDisabled}
-                >
-                  {tenQuest[currentQuest].correct}
-                </button>
+                {shuffledAnswers.map((answer) => (
+                  <button
+                    onClick={correctAnswer}
+                    disabled={btnDisabled}
+                    className={tenQuest.correct === answer ? "correct" : ""}
+                  >
+                    {answer}
+                  </button>
+                ))}
               </div>
               <div className="next-btn">
                 <button
@@ -114,14 +71,6 @@ export default function Questions(props) {
             </>
           )}
         </>
-      )}
-      {submit && (
-        <div className="score">
-          <h5>
-            You scored {score}/{tenQuest.length}
-          </h5>
-          <button onClick={retryBtn}>Retry</button>
-        </div>
       )}
     </div>
   );
